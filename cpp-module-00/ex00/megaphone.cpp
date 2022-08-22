2c6b7d5903fbc94f0fba86539dd77a98b1bda1ac
Kimhannah <skqkdldhf98@naver.com>
2022-08-22 20:17:51 +0900

    Complete ex00

--- cpp-module-00/ex00/megaphone.cpp
+++ cpp-module-00/ex00/megaphone.cpp
@@ -1,10 +1,28 @@
 #include <cctype>
 #include <iostream>
+#include <cstring>
 
-int main() {
-  char str1[] = "Hello I'm Hannah.";
+int main(int argc, char **argv) {
+	std::string str;
 
-  std::cout << str1 << std::endl;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND  UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	for (int i = 1; argv[i]; i++)
+	{
+		str = argv[i];
+		for (unsigned int j = 0; j < str.length(); j++)
+		{
+			if (str[j] >= 'a' && str[j] <= 'z')			// 소문자일 경우
+				str[j] = std::toupper(str[j]);
+			else if (str[j] >= 'A' && str[j] <= 'Z')
+				str[j] = std::tolower(str[j]);
+		}
+		std::cout << str;
+	}
+	std::cout << std::endl;
 
-  return (0);
+	return (0);
 }
