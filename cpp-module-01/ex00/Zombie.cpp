640899aed6e5a0373221711fc74c9c4e9b6d2fb2
hannkim <hannkim@student.42seoul.kr>
2022-08-25 10:41:25 +0900

    Complete ex00

--- cpp-module-01/ex00/Zombie.cpp
+++ cpp-module-01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: hannkim <hannkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 08:00:50 by hannkim           #+#    #+#             */
-/*   Updated: 2022/08/25 09:56:09 by hannkim          ###   ########.fr       */
+/*   Updated: 2022/08/25 10:39:18 by hannkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 Zombie::Zombie(std::string name)
 {
 	_name = name;
-	std::cout << _name << "is created.\n";
+	std::cout << _name << " is created.\n";
 }
 
 Zombie::~Zombie(void)
 {
-	std::cout << _name << "is destroyed.\n";
+	std::cout << _name << " is destroyed.\n";
 }
 
 void	Zombie::announce(void)
