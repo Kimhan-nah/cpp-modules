e708bdb88b2374a3c122e946b42dae38c58b1a08
hannkim <hannkim@student.42seoul.kr>
2022-08-26 08:00:21 +0900

    Complete ex06

--- cpp-module-01/ex06/Harl.cpp
+++ cpp-module-01/ex06/Harl.cpp
@@ -6,13 +6,57 @@
 /*   By: hannkim <hannkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 23:14:34 by hannkim           #+#    #+#             */
-/*   Updated: 2022/08/25 23:19:18 by hannkim          ###   ########.fr       */
+/*   Updated: 2022/08/26 07:56:58 by hannkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+#include <iostream>
 
 Harl::Harl() {}
 
 Harl::~Harl() {}
 
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger."
+				<< " I really do!\n\n";
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money." 
+				<< " You didn’t put enough bacon in my burger!"
+				<< " If you did, I wouldn’t be asking for more!\n\n";
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free."
+				<< " I’ve been coming for years whereas you started working here since last month.\n\n";
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n\n";
+}
+
+void	Harl::complain(std::string level)
+{
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    void (Harl::*fp[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	int	levelIndex = 5;
+	
+    for (int i = 0; i < 4; i++)
+	{
+		if (!level.compare(levels[i]))
+			levelIndex = i;
+		if (i >= levelIndex)
+		{
+			std::cout << "[ " << levels[i] << " ]\n";
+			(this->*fp[i])();		// function call
+		}
+    }
+	if (levelIndex == 5)
+		std::cout << "[ Probably complaining about insignificant problems ]\n";
+}
--- cpp-module-01/ex06/main.cpp
+++ cpp-module-01/ex06/main.cpp
@@ -6,7 +6,23 @@
 /*   By: hannkim <hannkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 23:14:40 by hannkim           #+#    #+#             */
-/*   Updated: 2022/08/25 23:14:40 by hannkim          ###   ########.fr       */
+/*   Updated: 2022/08/26 07:47:21 by hannkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	Harl harl;
+
+	if (argc != 2)
+	{
+		std::cout << "Error : Arguments\n";
+		exit(EXIT_FAILURE);
+	}
+	harl.complain(argv[1]);
+
+	return (0);
+}
