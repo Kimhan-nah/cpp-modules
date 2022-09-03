5d2c511f1789acd72cfa9cea3fb129897d9bd263
Kimhannah <skqkdldhf98@naver.com>
2022-09-03 18:30:35 +0900

    Complete ex00

---
+++ cpp-module-08/ex00/easyfind.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hannkim <hannkim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 14:35:42 by hannkim           #+#    #+#             */
+/*   Updated: 2022/09/03 18:26:23 by hannkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <exception>
+# include <algorithm>
+
+template <typename T>
+typename T::iterator easyfind(T &container, int i)
+{
+	typename T::iterator iter = std::find(container.begin(), container.end(), i);
+	if (iter == container.end())
+		throw std::logic_error("Error: Easyfind fail");
+	return (iter);
+}
+
+#endif
---
+++ cpp-module-08/ex00/main.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hannkim <hannkim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 14:34:33 by hannkim           #+#    #+#             */
+/*   Updated: 2022/09/03 18:29:58 by hannkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+#include <iostream>
+#include <vector>
+#include <list>
+
+int main()
+{
+	std::cout << "========= Vector Test ===========" << std::endl;
+	try {
+		std::vector<int>	v;
+		for (int i = 0; i < 10; i++) {
+			v.push_back(i);
+		}
+		std::vector<int>::iterator iter;
+		for (int i = 0; i < 10; i++) {
+			iter = easyfind(v, i);
+			// std::cout << "[" << i << "] " << *iter << std::endl;
+			std::cout << "success to find: " << *iter << std::endl;
+		}
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << "========= List Test ===========" << std::endl;
+	try {
+		std::list<char>	lst;
+		for (int i = 'a'; i < 'h'; i++) {
+			lst.push_back(i);
+		}
+		std::list<char>::iterator iter;
+		for (int i = 'a'; i < 'h'; i++) {
+			iter = easyfind(lst, i);
+			std::cout << "success to find: " << *iter << std::endl;
+		}
+
+		// exception!
+		iter = easyfind(lst, 'i');
+		std::cout << "success to find: " << *iter << std::endl;
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	return (0);
+}
